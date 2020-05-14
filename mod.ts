93af1b7ff34e15328366837f4a80d3ba4558307d
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 04:45:51 +0530

    :sparkles: Add package

--- mod.ts
+++ mod.ts
@@ -1,4 +1,12 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
+/** Invert key-value pair of an object */
+export function invert<T extends any>(obj: T): T {
+  const result: any = {};
+  for (const [key, value] of Object.entries(obj)) {
+    result[value] = key;
+  }
+  for (const symbol of Object.getOwnPropertySymbols(obj)) {
+    const value = obj[symbol];
+    result[value] = symbol;
+  }
+  return result as T;
 }
